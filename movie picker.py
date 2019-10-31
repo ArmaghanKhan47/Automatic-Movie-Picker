ba4e7d2351aec205f306fe22440777d6f677b79c
Armaghan Khan <38203727+ArmaghanKhan47@users.noreply.github.com>
2019-10-31 16:03:21 +0500

    Update movie picker.py

--- movie picker.py
+++ movie picker.py
@@ -4,6 +4,7 @@ movies_path = os.getcwd() #path of folder where all movies are stored
 check = True
 choice = False
 
+#function to select movie
 def movieSelector(path):
     os.chdir(path)
     movies_list = os.listdir()
@@ -11,6 +12,33 @@ def movieSelector(path):
     
     selected_movie = random.randrange(0, number_of_movies)
     return movies_list[selected_movie]
+
+#function to check that selected name is a file or directory
+def checkType(selected_name):
+    if (os.path.isdir(selected_name)):
+        result = True
+    elif (os.path.isfile(selected_name)):
+        result = False
+    return result
+
+#function to start the movie
+def startMovie(selected_name):
+    file_type = checkType(selected_name)
+    
+    if (file_type):
+        os.chdir(selected_name)
+        files = os.listdir()
+        subfiles = len(files)
+        for x in range(0, subfiles):
+            fileextension = os.path.splitext(files[x])[1]
+            if (fileextension == ".mp4" or fileextension == ".mkv"):
+                os.system(".\\" + "\"" + files[x] + "\"")
+                os.system("exit")
+                break
+    else:
+        fileextension = os.path.splitext(selected_name)[1]
+        if (fileextension == ".mp4" or fileextension == ".mkv"):
+            os.system(".\\" + "\"" + selected_name + "\"")
 
 print("Hi! I am here to help you in picking up the movie")
 input("Press any key so that I can start helping right now")
@@ -29,19 +57,7 @@ while check:
     
     ans = input("Do you want me to start the movie press Y/y: ")
     if (ans == 'Y' or ans == 'y'):
-        choice = True
-        
-    if (choice == True):
-       os.chdir(name)
-       files = os.listdir()
-       subfiles = len(files)
-       for x in range(0, subfiles):
-           fileextension = os.path.splitext(files[x])[1]
-           if (fileextension == ".mp4"):
-               os.system(".\\" + files[x])
-               os.system("exit")
-               break
-       break    
+        startMovie(name)
         
     letter = input("To Shutdown Press n/N or To run again press any key: ")
     if (letter == 'n' or letter == 'N'):
